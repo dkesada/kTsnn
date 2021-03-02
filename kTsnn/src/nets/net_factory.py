7de6341f6638040949c564be421056996e572691
dkesada <dkesada@gmail.com>
2021-03-02 15:02:17 +0100

    update and resume

--- kTsnn/src/nets/net_factory.py
+++ kTsnn/src/nets/net_factory.py
@@ -2,24 +2,50 @@ from abc import ABC, abstractmethod
 
 
 class TsNetwork(ABC):
+    """
+    Interface for different time-series neural networks
 
-    def __init__(self, epochs, batch):
+    Attributes
+    ----------
+
+    epochs: int
+        number of epochs for training
+    batch: int
+        size of the batch
+
+    Methods
+    -------
+
+    train_net(x_train, y_train, **kwargs)
+        trains the NN with its own implemented method
+    fit_net(x_train, y_train, x_test, y_test, **kwargs)
+        fits the NN with its own implemented method
+    train_net(x_train, y_train, **kwargs)
+        trains the NN with its own implemented methods
+    predict(x_test, y_test, **kwargs)
+        predicts a dataset with the model
+    predict_long_term(x_test, y_test, obj_var, ini, length, **kwargs)
+        predicts a time-series in the long term
+
+    """
+
+    def __init__(self, epochs, batch, **kwargs):
         self._epochs = epochs
         self._batch = batch
         self._model = None
 
     @abstractmethod
-    def train_net(self, x_train, y_train):
+    def train_net(self, x_train, y_train, **kwargs):
         pass
 
     @abstractmethod
-    def fit_net(self, x_train, y_train, x_test, y_test):
+    def fit_net(self, x_train, y_train, x_test, y_test, **kwargs):
         pass
 
     @abstractmethod
-    def predict(self, x_test, y_test, verbose):
+    def predict(self, x_test, y_test, **kwargs):
         pass
 
     @abstractmethod
-    def predict_long_term(self, x_test, y_test, verbose):
+    def predict_long_term(self, x_test, y_test, obj_var, ini, length, **kwargs):
         pass
--- kTsnn/src/nets/tdnn.py
+++ kTsnn/src/nets/tdnn.py
@@ -1,8 +1,8 @@
-from keras.models import Model
-from keras.layers import Dense, Input, Dropout, Conv2D, Conv1D, Flatten
-from keras.optimizers import RMSprop, sgd, adam
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input, Dropout, Conv2D, Conv1D, Flatten
+from tensorflow.keras.optimizers import RMSprop, SGD, Adam
 import pandas as pd
-from kTsnn.src.utils import *
+from ..utils import *
 from .net_factory import TsNetwork
 
 
@@ -13,7 +13,7 @@ class TDNN(TsNetwork):
     # Define the structure of the TDNN and compile the model
     # Not parametrized for now, changes are hard coded inside
     # I should resort to some structure-storing keras method
-    def train_net(self, x_train, y_train):
+    def train_net(self, x_train, y_train, **kwargs):
         inputs = Input(shape=x_train.shape[1:])
         x = Dense(528, activation='relu')(inputs)
         x = Dense(274, activation='relu')(x)
@@ -23,11 +23,11 @@ class TDNN(TsNetwork):
         #x = Dense(8, activation='relu')(x)
         pred = Dense(pd.DataFrame(y_train).shape[1])(x)
         self._model = Model(inputs=inputs, outputs=pred)
-        optim = adam(lr=0.001, beta_1=0.9, beta_2=0.999)
+        optim = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
         self._model.compile(loss='mae', optimizer=optim, metrics=['mae'])
 
     # Fit the keras model to the training data and validate the results
-    def fit_net(self, x_train, y_train, x_test, y_test):
+    def fit_net(self, x_train, y_train, x_test, y_test, **kwargs):
         log = self._model.fit(x_train, y_train, batch_size=self._batch,
                               epochs=self._epochs, validation_data=(x_test, y_test))
         plot_train_val_loss(log)
@@ -46,11 +46,11 @@ class TDNN(TsNetwork):
 
         return evidence
 
-    def predict(self, x_test, y_test, verbose):
+    def predict(self, x_test, y_test, **kwargs):
         return self._model.predict(x_test)
 
     # Function to do long term forecasting with a trained TDNN
-    def predict_long_term(self, x_test, y_test, obj_var):
+    def predict_long_term(self, x_test, y_test, obj_var, ini, length, **kwargs):
         path = []
         evidence = x_test.iloc[0:1]
         for i in range(x_test.shape[0]):
