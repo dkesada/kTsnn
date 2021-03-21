dbbc5e8247d7529887869f1ed8127b0554f6848f
CIG <dkesada@gmail.com>
2021-03-21 14:25:00 +0100

    LSTM added. Stick to convolutional

---
+++ kTsnn/src/nets/lstm.py
@@ -0,0 +1,124 @@
+import tensorflow as tf
+from .net_factory import TsNetwork
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+
+
+class AutoLSTM(TsNetwork, tf.keras.Model):
+    def __init__(self, epochs, window, num_features, units=32, out_steps=50):
+        TsNetwork.__init__(self, epochs, window, None)
+        tf.keras.Model.__init__(self)
+        self.out_steps = out_steps
+        self._num_features = num_features
+        self.units = units
+        self.lstm_cell = tf.keras.layers.LSTMCell(units)
+        # Also wrap the LSTMCell in an RNN to simplify the `warmup` method.
+        self.lstm_rnn = tf.keras.layers.RNN(self.lstm_cell, return_state=True)
+        self.dense = tf.keras.layers.Dense(num_features)
+
+    def warmup(self, inputs):
+        # inputs.shape => (batch, time, features)
+        # x.shape => (batch, lstm_units)
+        x, *state = self.lstm_rnn(inputs)
+
+        # predictions.shape => (batch, features)
+        prediction = self.dense(x)
+        return prediction, state
+
+    # Define the loss, optimizer and metrics and compile the model
+    def train_net(self, loss=tf.losses.MeanSquaredError(), opt=tf.optimizers.Adam(), metrics=[tf.metrics.MeanAbsoluteError()]):
+        self.compile(loss=loss, optimizer=opt, metrics=metrics)
+
+    # Fit the keras model to the training data and validate the results
+    def fit_net(self, patience=5, show_plot=True):
+        if patience is not None:
+            early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=patience,mode='min')
+            log = self.fit(self._window.train, epochs=self._epochs, validation_data=self._window.val,
+                                  callbacks=[early_stopping])
+        else:
+            log = self.fit(self._window.train, epochs=self._epochs, validation_data=self._window.val)
+        if show_plot:
+            self._plot_train_val_loss(log)
+
+        return log
+
+    def predict(self, dt, obj_var, show_plot):
+        return None
+
+    def predict_long_term(self, dt, obj_var, length, show_plot=True):
+        if len(dt) < self._window.input_width:
+            raise ValueError(f'At least {self._window.input_width} previous instants have to be provided')
+
+        dt_ini = dt.iloc[0:self._window.input_width, :]
+        dt_empty = DataFrame(None, index=range(self._window.label_width), columns=dt_ini.columns)
+        dt_ini = dt_ini.append(dt_empty)
+        prep_dt = self._window.make_dataset(dt_ini)
+        inputs, labels = next(iter(prep_dt))
+
+        return self.call(inputs, None)
+
+    # Function to do long term forecasting with a trained TDNN
+    def call(self, inputs, training):
+        # Use a TensorArray to capture dynamically unrolled outputs.
+        predictions = []
+        # Initialize the lstm state
+        prediction, state = self.warmup(inputs)
+
+        # Insert the first prediction
+        predictions.append(prediction)
+
+        # Run the rest of the prediction steps
+        for n in range(1, self.out_steps):
+            # Use the last prediction as input.
+            x = prediction
+            # Execute one lstm step.
+            x, state = self.lstm_cell(x, states=state,
+                                      training=training)
+            # Convert the lstm output to a prediction.
+            prediction = self.dense(x)
+            # Add the prediction to the output
+            predictions.append(prediction)
+
+        # predictions.shape => (time, batch, features)
+        predictions = tf.stack(predictions)
+        # predictions.shape => (batch, time, features)
+        predictions = tf.transpose(predictions, [1, 0, 2])
+
+        return predictions
+
+    def _default_model(self):
+        return None
+
+    def _plot_predictions(self, inputs, labels, preds, obj_var):
+        plt.figure()
+        initial_line, = plt.plot(range(self._window.input_width),
+                                 inputs[0, :, self._window.column_indices.get(obj_var)], label='Initial values')
+        real_line, = plt.plot(range(self._window.input_width - 1, self._window.total_window_size - 1),
+                              labels[0, :, self._window.label_columns_indices.get(obj_var)], label='Real values')
+        pred_line, = plt.plot(range(self._window.input_width - 1, self._window.total_window_size - 1),
+                              preds[0, :, self._window.label_columns_indices.get(obj_var)],
+                              label='Predicted values')
+        plt.legend(handles=[initial_line, real_line, pred_line])
+        plt.ylabel(obj_var)
+        plt.xlabel('Time (h)')
+        plt.show()
+
+    def _plot_predictions_long_term(self, dt, path, obj_var, length):
+        plt.figure()
+        initial_line, = plt.plot(range(self._window.input_width),
+                                 dt[obj_var][0:self._window.input_width], label='Initial values')
+        real_line, = plt.plot(range(self._window.input_width - 1, length + self._window.input_width - 1),
+                              dt[obj_var][range(self._window.input_width, length+self._window.input_width)], label='Real values')
+        pred_line, = plt.plot(range(self._window.input_width - 1, length),
+                              path[range(self._window.input_width-1, length),
+                                   self._window.label_columns_indices.get(obj_var)],label='Predicted values')
+        plt.legend(handles=[initial_line, real_line, pred_line])
+        plt.ylabel(obj_var)
+        plt.xlabel('Time (h)')
+        plt.show()
+
+    # Move the evidence one time slice and introduce the new predictions as evidence
+    @staticmethod
+    def __move_evidence(evidence, particles):
+
+        return None
--- kTsnn/src/test.py
+++ kTsnn/src/test.py
@@ -1,5 +1,6 @@
 from kTsnn.src.utils import *
 from kTsnn.src.nets.cnn import Conv1dnn
+from kTsnn.src.nets.lstm import AutoLSTM
 from kTsnn.src.nets.window_gen import WindowGenerator
 import tensorflow as tf
 import os
@@ -26,19 +27,23 @@ if __name__ == '__main__':
     num_features = dt_train.shape[1]
 
     # Forecasting into the future
-    OUT_STEPS = 20
+    OUT_STEPS = 7
+    CONV_WIDTH = 7
     INPUT_WIDTH = 7
     multi_window = WindowGenerator(input_width=INPUT_WIDTH, label_width=OUT_STEPS, shift=OUT_STEPS,
                          dt_train=dt_train, dt_test=dt_test, dt_val=dt_val,
                          label_columns=dt_train.columns)  #info['obj_var'])
-    CONV_WIDTH = 7
-    ini = 0
 
-    model = Conv1dnn(MAX_EPOCHS, multi_window, num_features, conv_width=CONV_WIDTH, out_steps=OUT_STEPS)
+
+    #model = Conv1dnn(MAX_EPOCHS, multi_window, num_features, model=None, conv_width=CONV_WIDTH, out_steps=OUT_STEPS)
+    model = AutoLSTM(MAX_EPOCHS, multi_window, num_features, units=32, out_steps=OUT_STEPS)
     model.train_net()
     model.fit_net(patience=5)
+
+    ini = 0
+
     #pred = model.predict(dt_test.loc[ini:(ini+INPUT_WIDTH+OUT_STEPS), :], obj_var=info['obj_var'][0])
-    path = model.predict_long_term(dt_test, obj_var=info['obj_var'][0], length=77)
+    path = model.predict_long_term(dt_test, obj_var=info['obj_var'][0], length=OUT_STEPS)
 
 
 
