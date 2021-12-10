e3ef2893df0230cfc35ca98a5557f7869c4c002b
CIG <dkesada@gmail.com>
2021-12-10 14:19:13 +0100

    bug fix in moving window

--- kTsnn/src/nets/lstm.py
+++ kTsnn/src/nets/lstm.py
@@ -92,10 +92,6 @@ class AutoLSTM(TsNetwork):
             else:
                 dt_ini.iloc[0:self._window.input_width, :] = preds[0, :, :]
 
-            dt_ini.iloc[0:self._window.input_width, :] = \
-                preds[0, range(self._window.label_width - self._window.input_width,
-                               self._window.label_width), :]  # Move the predictions as evidence
-
         if show_plot:
             self._plot_predictions_long_term(dt, path, obj_var, length)
 
